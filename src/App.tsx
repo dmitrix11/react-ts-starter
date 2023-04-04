6a73559560a7547a145485966f2770ddb47f2ac1
dmitrix11 <20766924+dmitrix11@users.noreply.github.com>
2023-04-04 08:05:28 -0400

    Initial Commit 🫡

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='bg-zinc-950 grid items-center justify-center min-h-screen'>
+      <h1 className='text-3xl font-bold text-zinc-50'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
